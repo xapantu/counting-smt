6db14df68d83986ee1cd7c4ae8b38653ba1131e3
Lucas Baudin <lucas.baudin@ens.fr>
2016-07-05 10:47:46 -0700

    remove commented code

--- tests.ml
+++ tests.ml
@@ -20,34 +20,6 @@ let test_domain_neg _ =
   assert_equal  (snd @@ List.hd @@ dom_neg) (Expr (IValue 6), Expr (IValue 8));
   ()
 
-(* let _ = LA_SMT.set_verbose true *)
-
-(* let suite = *)
-(*   "suite" >::: *)
-(*   [ *)
-(* "test1" >:: test_file "test1.smt" (assert_equal "sat\ny = 2\nx = 3\n"); *)
-(* "test2" >:: test_file "test2.smt" (assert_equal "unsat\n"); *)
-(* "test3" >:: test_file "test3.smt" (assert_equal "sat\n"); *)
-(* "test_quant_sum" >:: test_file "test_quant_sum.smt" (assert_equal "unsat\n"); *)
-(* "test_sum" >:: test_file "test_sum.smt" (assert_equal "unsat\n"); *)
-(* "test_shadowing" >:: test_file "shadowing.smt" (assert_equal "sat\ny = 1\nx = 1\n"); *)
-(* "test_lower" >:: test_file "test_lower.smt" (assert_equal "sat\ny = 0\nx = 1\n"); *)
-(* "test_birecursion" >:: test_file "birecursion.smt" (assert_equal "sat\ny = 0\nx = 10\n"); *)
-(* "test4" >:: test_file "test4.smt" (fun _ -> ()); *)
-(* "vars" >:: test_file "vars.smt" (fun _ -> ()); *)
-(* "constants" >:: test_file "constants.smt" (assert_equal "sat\ns2 = 1\ns1 = 2\n"); *)
-(* "novals" >:: test_file "novars.smt" (assert_equal "sat\n"); *)
-(* "strict_comparisons" >:: test_file "strict_comparison.smt" (assert_equal "sat\ns3 = 0\ns2 = 1\ns1 = 2\n"); *)
-(* "not" >:: test_file "not.smt" (assert_equal "sat\nx = 10\n"); *)
-(* "multiple_cards.smt" >:: test_file "multiple_card.smt" (assert_equal "unsat\n"); *)
-    
-(* "test_arrays" >:: test_file "test_arrays.smt" (assert_equal "sat\n"); *)
-(* "test_arrays2" >:: test_file "test_arrays2.smt" (assert_equal "unsat\n"); *)
-(* "test_arrays3" >:: test_file "test_arrays3.smt" (assert_equal "sat\n"); *)
-(* "test_arrays4" >:: test_file "test_arrays4.smt" (assert_equal "unsat\n"); *)
-(* "test_arrays5" >:: test_file "test_arrays5.smt" (assert_equal "unsat\n"); *)
-(* ] *)
-
 let test1 = "test_domain_neg" >:: test_domain_neg
 
 let () = run_test_tt_main test1
